dfaa6e4dfc5af8196935742d037293e4680f346c
kishiba <y.kishiba1266@gmail.com>
2025-09-06 23:40:47 +0900

    Update README

--- app/src/main/java/com/example/iods/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/iods/ui/home/HomeScreen.kt
@@ -3,7 +3,6 @@ package com.example.iods.ui.home
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,7 +19,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Expand
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
