b51c4f07a3cb68a234e26e0521eca2103845dfe3
kishiba <y.kishiba1266@gmail.com>
2025-09-27 01:08:12 +0900

    Update Gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.1.21-2.0.2"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3"
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.iods"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -57,23 +57,23 @@ android {
 dependencies {
 
     //Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2025.08.01"))
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.09.01"))
+    implementation("androidx.activity:activity-compose:1.11.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-    implementation("androidx.navigation:navigation-compose:2.9.3")
-    implementation("androidx.compose.animation:animation-android:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.compose.animation:animation-android:1.9.2")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
-    implementation("androidx.compose.runtime:runtime:1.9.0")
+    implementation("androidx.compose.runtime:runtime:1.9.2")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
@@ -81,13 +81,13 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     //Room
-    val roomVersion = "2.7.2"
+    val roomVersion = "2.8.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.core:core-ktx:1.17.0")
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
 
     // Testing
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
 }
